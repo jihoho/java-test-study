0347a92b68d6c30192ca8ed46b80a67132fa1721
jihoho <xkftn94@naver.com>
2021-06-11 20:03:15 +0900

    Initial commit

---
+++ src/test/java/ch01/ScoreCollectionTest.java
@@ -0,0 +1,24 @@
+package ch01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: hojun
+ * Date: 2021-06-11 Time: 오후 8:01
+ */
+public class ScoreCollectionTest {
+
+    @Test
+    public void test() throws Exception{
+        // given
+        fail("Not yet implemented");
+        // when
+
+        // then
+    }
+
+
+}
